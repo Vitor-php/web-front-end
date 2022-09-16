e32857f7a43e9d6c60b186a75b755af6475fd34a
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-15 23:14:09 -0300

    fix: changes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo-nlw-esports.svg';
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="Logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20 ">
+          Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.
+      </h1>
+
+   
+   
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full p-16 pb-4 px-4 bg-game-gradient">
+            <strong className="font-bold">League of Legends</strong>
+            <span>4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/game-2.png" alt="" />
+
+
+        </a>
+        <a href="" className="relative">
+          <img src="/game-3.png" alt="" />
+
+
+        </a>
+        <a href="" className="relative">
+          <img src="/game-4.png" alt="" />
+
+
+        </a>
+        <a href="" className="relative">
+          <img src="/game-5.png" alt="" />
+
+
+        </a>
+        <a href="" className="relative">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
+    </div>
+    
+  )
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('./background-galaxy.png')",
-      }
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 62.97%, #E1D55D 22.54%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 0%, 67.08%)',
+      }, 
     },
   },
   plugins: [],
