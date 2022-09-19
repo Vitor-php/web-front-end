1ecb9b163f79a91d8e0016669267928f02e5c370
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-19 15:27:13 -0300

    fix: changes in form

--- src/App.tsx
+++ src/App.tsx
@@ -150,15 +150,23 @@ function App() {
                   </div>
                 </div>
               </div>
-              <div>
+              <div className="mt-2 flex gap-2 text-sm">
                 <Input type="checkbox" />
                 Costumo me conectar ao chat de voz
               </div>
 
-              <footer>
-                <button>Cancelar</button>
-                <button type="submit">
-                  <GameController />
+              <footer className="mt-4 flex justify-end gap-4">
+                <Dialog.Close
+                  type="button"
+                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
+                >
+                  Cancelar
+                </Dialog.Close>
+                <button
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                  type="submit"
+                >
+                  <GameController size={24} />
                   Encontrar duo
                 </button>
               </footer>
